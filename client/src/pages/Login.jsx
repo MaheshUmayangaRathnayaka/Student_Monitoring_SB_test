2c7d96bafa653366ed518f405d20952bb1563da2
MaheshUmayangaRathnayaka <maheshumayangarathnayaka@gmail.com>
2026-02-15 11:20:51 +0530

    new one

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -41,7 +41,7 @@ const Login = () => {
     <div className="auth-container">
       <div className="auth-card">
         <h1>Student Performance System</h1>
-        <h2>Mahesh</h2>
+        <h2>Maheshr</h2>
         
         {error && <div className="error-message">{error}</div>}
         
